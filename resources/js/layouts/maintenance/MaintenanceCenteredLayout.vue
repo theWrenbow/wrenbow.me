7f849de9981a177f41bc7f36d2f8b07eb4be741d
wrenbow🌈🕊️ <wren@wrenbow.me>
2026-03-22 18:27:19 -0600

    Add browser title

--- resources/js/layouts/maintenance/MaintenanceCenteredLayout.vue
+++ resources/js/layouts/maintenance/MaintenanceCenteredLayout.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
 type Props = {
+    title?: string;
     heading?: string;
     buttons?: ButtonItem[];
 };
@@ -10,6 +12,7 @@ withDefaults(defineProps<Props>(), {
 });
 </script>
 <template>
+    <Head :title="title" />
     <main class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10 select-none font-mono">
         <div class="w-full max-w-sm">
             <div class="flex flex-col gap-8">
